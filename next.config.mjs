6a12b6d976480fea6e0a459ae84e9eed4587a7ac
Andre <yiran@yahoo.co.jp>
2025-12-13 10:56:22 +0900

    chore: enable next-auth debug mode

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    serverComponentsExternalPackages: ['prisma', '@prisma/client'],
+  },
 };
 
 export default nextConfig;
